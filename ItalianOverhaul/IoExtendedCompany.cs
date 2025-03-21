f5f1f3ac5a3a71ed07ea2139ef3c4dac8589089b
PaoloBitta <cgbvcb73@gmail.com>
2025-03-21 22:33:20 +0100

    Started implementation of extended `Company` class

---
+++ ItalianOverhaul/IoExtendedCompany.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItalianOverhaul
+{
+    internal class IoExtendedCompany : Company
+    {
+        IoExtendedCompany() { }
+
+        public enum IoCompanyType
+        {
+            SocietaResponsabilitaLimitata,          // SRL - pretty much the same as a LLC.
+            SocietaPerAzioni,                       // SpA - public company.
+        }
+
+        internal Dictionary<IoCompanyType, string> IoCompanyTypeNames = new Dictionary<IoCompanyType, string>
+        {
+            { IoCompanyType.SocietaResponsabilitaLimitata, "SRL" },
+            { IoCompanyType.SocietaPerAzioni, "S.p.A." },
+        };
+
+        public IoCompanyType CompanyType { get; set; }
+
+        public bool CanTransitionToSpa()
+        {
+            return Money > 50000;
+        }
+
+        public void TransitionToSpa()
+        {
+            if (CanTransitionToSpa())
+            {
+               MakeTransaction(-50000, TransactionCategory.Legal, "Transition to S.p.A.");
+                CompanyType = IoCompanyType.SocietaPerAzioni;
+            }
+
+            // TODO: Add a notification for the player (gotta figure out how to do that).
+        }
+    }
+}
