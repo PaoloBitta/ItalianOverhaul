379f0b8166b71ea0135deac5afe6cc8a2ee2beeb
PaoloBitta <cgbvcb73@gmail.com>
2025-03-21 21:28:33 +0100

    Add project files.

---
+++ ItalianOverhaul/ItalianOverhaul.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ItalianOverhaul
+{
+    internal class ItalianOverhaulModMeta : ModMeta
+    {
+        public override string Name => "Italian Overhaul";
+
+        public override void ConstructOptionsScreen(RectTransform parent, bool inGame)
+        {
+            Text text = WindowManager.SpawnLabel();
+            text.text = @"
+                Italian Overhaul is a mod for Software Inc. which brings the Authentic Italian™ software house experience.
+                Currently in development, not much to see here!!
+            ";
+            WindowManager.AddElementToElement(text.gameObject, parent.gameObject, new Rect(0f, 0f, 400f, 128f),
+                new Rect(0f, 0f, 0f, 0f));
+        }
+    }
+}
